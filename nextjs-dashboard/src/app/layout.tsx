0aa13991c7a8c6d77ee4f5823e52da25b13d2f88
JaredLarios <lar21038@byui.edu>
2025-09-24 20:17:23 -0600

    fix: header on layout.

--- nextjs-dashboard/src/app/layout.tsx
+++ nextjs-dashboard/src/app/layout.tsx
@@ -29,11 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <Header />
       <body className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen`}>
         <main className="flex-grow">{children}</main>
         <Footer />
-
       </body>
     </html>
   );
