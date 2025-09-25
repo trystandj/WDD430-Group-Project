e0c4ae2edb5c4730699fe5f31f56953725347789
trystandj <trystandj@gmail.com>
2025-09-24 22:17:21 -0400

    Fixed for main branch

--- nextjs-dashboard/src/app/page.tsx
+++ nextjs-dashboard/src/app/page.tsx
@@ -11,21 +11,7 @@ export default function Home() {
 
         <BusinessSpotlight />
 
-      <div className={styles.container} />
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+
 
 
       </main>
