68c604ed63b16cdb7f06dc5e937530d0886de6ae
JaredLarios <lar21038@byui.edu>
2025-10-08 20:57:42 -0600

    fix: header position.

--- nextjs-dashboard/src/app/items/[itemId]/page.tsx
+++ nextjs-dashboard/src/app/items/[itemId]/page.tsx
@@ -4,7 +4,7 @@ import "../../(landingComponents)/sellers/[id]/seller-detail.css";
 import ItemDetail from '@/app/(landingComponents)/items/ItemDetail'
 
 export default async function ItemPage({ params }: { params: { itemId: string } }) {
-  const { itemId } = await params
+  const { itemId } = params;
   const idNum = Number(itemId)
   if (Number.isNaN(idNum)) {
     return <div className="p-6">Invalid item id</div>
