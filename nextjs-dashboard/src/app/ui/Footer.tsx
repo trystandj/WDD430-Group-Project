f23f72097afbf84b6634434fa9e0dabae5f6c6b0
chighwood <hig23003@byui.edu>
2025-10-14 13:13:25 -0500

    Footer changes Adds footer to all pages

---
+++ nextjs-dashboard/src/app/ui/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+    
+      <div className={styles.footer_copyright}><p>© 2025 All rights reserved.</p></div>
+
+    </footer>
+  )
+}
